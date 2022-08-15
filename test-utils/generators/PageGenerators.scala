529265854b6ef250eb596fc6cc1b5439898a35f2
Michael Wolfendale <4563722+wolfendale@users.noreply.github.com>
2022-08-15 14:41:48 +0100

    Scaffold Add Period Page

--- test-utils/generators/PageGenerators.scala
+++ test-utils/generators/PageGenerators.scala
@@ -21,6 +21,9 @@ import pages._
 
 trait PageGenerators {
 
+  implicit lazy val arbitraryAddPeriodPage: Arbitrary[AddPeriodPage.type] =
+    Arbitrary(AddPeriodPage)
+
   implicit lazy val arbitraryPeriodPage: Arbitrary[PeriodPage.type] =
     Arbitrary(PeriodPage)
 
--- test-utils/generators/UserAnswersEntryGenerators.scala
+++ test-utils/generators/UserAnswersEntryGenerators.scala
@@ -24,6 +24,14 @@ import play.api.libs.json.{JsValue, Json}
 
 trait UserAnswersEntryGenerators extends PageGenerators with ModelGenerators {
 
+  implicit lazy val arbitraryAddPeriodUserAnswersEntry: Arbitrary[(AddPeriodPage.type, JsValue)] =
+    Arbitrary {
+      for {
+        page  <- arbitrary[AddPeriodPage.type]
+        value <- arbitrary[Boolean].map(Json.toJson(_))
+      } yield (page, value)
+    }
+
   implicit lazy val arbitraryPeriodUserAnswersEntry: Arbitrary[(PeriodPage.type, JsValue)] =
     Arbitrary {
       for {
