b03b068d16e8433342520e26780e6c85fe260c91
Michael Wolfendale <4563722+wolfendale@users.noreply.github.com>
2022-08-16 16:54:52 +0100

    Update content for Was Child Under 12 Years Old Page

--- test/controllers/WasChildUnder12YearsOldControllerSpec.scala
+++ test/controllers/WasChildUnder12YearsOldControllerSpec.scala
@@ -18,12 +18,12 @@ package controllers
 
 import base.SpecBase
 import forms.WasChildUnder12YearsOldFormProvider
-import models.{NormalMode, UserAnswers}
+import models.{ChildName, NormalMode, UserAnswers}
 import navigation.{FakeNavigator, Navigator}
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
 import org.scalatestplus.mockito.MockitoSugar
-import pages.WasChildUnder12YearsOldPage
+import pages.{ChildNamePage, WasChildUnder12YearsOldPage}
 import play.api.inject.bind
 import play.api.mvc.Call
 import play.api.test.FakeRequest
@@ -42,50 +42,47 @@ class WasChildUnder12YearsOldControllerSpec extends SpecBase with MockitoSugar {
 
   lazy val wasChildUnder12YearsOldRoute = routes.WasChildUnder12YearsOldController.onPageLoad(NormalMode).url
 
+  val childName = ChildName("Foo", "Bar")
+  val minimalUserAnswers = emptyUserAnswers.set(ChildNamePage, childName).success.value
+
   "WasChildUnder12YearsOld Controller" - {
 
     "must return OK and the correct view for a GET" in {
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      val application = applicationBuilder(userAnswers = Some(minimalUserAnswers)).build()
 
       running(application) {
         val request = FakeRequest(GET, wasChildUnder12YearsOldRoute)
-
         val result = route(application, request).value
-
         val view = application.injector.instanceOf[WasChildUnder12YearsOldView]
 
         status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form, NormalMode)(request, messages(application)).toString
+        contentAsString(result) mustEqual view(form, childName, NormalMode)(request, messages(application)).toString
       }
     }
 
     "must populate the view correctly on a GET when the question has previously been answered" in {
 
-      val userAnswers = UserAnswers(userAnswersId).set(WasChildUnder12YearsOldPage, true).success.value
-
+      val userAnswers = minimalUserAnswers.set(WasChildUnder12YearsOldPage, true).success.value
       val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
 
       running(application) {
         val request = FakeRequest(GET, wasChildUnder12YearsOldRoute)
-
         val view = application.injector.instanceOf[WasChildUnder12YearsOldView]
-
         val result = route(application, request).value
 
         status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form.fill(true), NormalMode)(request, messages(application)).toString
+        contentAsString(result) mustEqual view(form.fill(true), childName, NormalMode)(request, messages(application)).toString
       }
     }
 
     "must redirect to the next page when valid data is submitted" in {
 
       val mockSessionRepository = mock[SessionRepository]
-
       when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
 
       val application =
-        applicationBuilder(userAnswers = Some(emptyUserAnswers))
+        applicationBuilder(userAnswers = Some(minimalUserAnswers))
           .overrides(
             bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
             bind[SessionRepository].toInstance(mockSessionRepository)
@@ -96,7 +93,6 @@ class WasChildUnder12YearsOldControllerSpec extends SpecBase with MockitoSugar {
         val request =
           FakeRequest(POST, wasChildUnder12YearsOldRoute)
             .withFormUrlEncodedBody(("value", "true"))
-
         val result = route(application, request).value
 
         status(result) mustEqual SEE_OTHER
@@ -106,21 +102,18 @@ class WasChildUnder12YearsOldControllerSpec extends SpecBase with MockitoSugar {
 
     "must return a Bad Request and errors when invalid data is submitted" in {
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      val application = applicationBuilder(userAnswers = Some(minimalUserAnswers)).build()
 
       running(application) {
         val request =
           FakeRequest(POST, wasChildUnder12YearsOldRoute)
             .withFormUrlEncodedBody(("value", ""))
-
         val boundForm = form.bind(Map("value" -> ""))
-
         val view = application.injector.instanceOf[WasChildUnder12YearsOldView]
-
         val result = route(application, request).value
 
         status(result) mustEqual BAD_REQUEST
-        contentAsString(result) mustEqual view(boundForm, NormalMode)(request, messages(application)).toString
+        contentAsString(result) mustEqual view(boundForm, childName, NormalMode)(request, messages(application)).toString
       }
     }
 
@@ -130,7 +123,19 @@ class WasChildUnder12YearsOldControllerSpec extends SpecBase with MockitoSugar {
 
       running(application) {
         val request = FakeRequest(GET, wasChildUnder12YearsOldRoute)
+        val result = route(application, request).value
 
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "must redirect to Journey Recovery for a GET if no child name is found" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, wasChildUnder12YearsOldRoute)
         val result = route(application, request).value
 
         status(result) mustEqual SEE_OTHER
@@ -146,7 +151,21 @@ class WasChildUnder12YearsOldControllerSpec extends SpecBase with MockitoSugar {
         val request =
           FakeRequest(POST, wasChildUnder12YearsOldRoute)
             .withFormUrlEncodedBody(("value", "true"))
+        val result = route(application, request).value
 
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "must redirect to Journey Recovery for a POST if no child name is found" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, wasChildUnder12YearsOldRoute)
+            .withFormUrlEncodedBody(("value", "true"))
         val result = route(application, request).value
 
         status(result) mustEqual SEE_OTHER
